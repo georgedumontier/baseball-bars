c802ff2dbe8e9e2962b06611354ecd9bf21a06d3
George DuMontier <georgedumontier@Georges-iMac.local>
2015-02-06 13:45:23 -0600

    added homeruns and stolen bases

--- js/script.js
+++ js/script.js
@@ -16,9 +16,24 @@ function getData() {
 
 function drawChart() {
 	$.each(playerData.stats, function(i,item){
-		    console.log(item);
-		    var width = item.HR * 10;
-		    $(".chart").append('<div class="bar" style="width: '+ width + 'px">' + item.HR + '<div>');
+	    console.log(item);
+	    var width = item.HR * 10;
+	    $(".chart").append(
+	    	'<div class="col-md-12 bar-container">' +
+	    		'<div class="year">' + item.year + '</div>' +
+	    		'<div class="bar" style="width: '+ width + 'px"></div>' + 
+	    		'<div class="total">' + item.HR + '<div>' +
+	    	'</div>'
+	    );
+
+	    var sbWidth = item.SB * 10;
+	    $(".chart2").append(
+	    	'<div class="col-md-12 bar-container">' +
+	    		'<div class="year">' + item.year + '</div>' +
+	    		'<div class="bar" style="width: '+ sbWidth + 'px"></div>' + 
+	    		'<div class="total">' + item.SB + '<div>' +
+	    	'</div>'
+	    );
 
 	});
 }
